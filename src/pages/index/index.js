786188807a90afa393f4ac9685e2f36698db8783
吴凯 <wukaiismyh@gmail.com>
2019-09-23 21:36:03 +0800

    【*】接口调试

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -18,7 +18,42 @@ $(function() {
   $("#product").click(function() {
     console.log("在线报名");
     $(".models").attr("id", "myModal");
-    $(".modal-body").html("联系人和联系电话必填！");
+    // $(".modal-body").html("联系人和联系电话必填！");
+  });
+  // 提交在线报名数据
+  $(".submit").click(function() {
+    $(".models").attr("id", "");
+    if ($("#name").val() && $("#mobile").val()) {
+      $(".models").attr("id", "myModal");
+      var datas = $("form").serialize();
+      var targetUrl = "/yanghua_edu/api/other_module/online_sign/";
+      $.ajax({
+        type: "post",
+        url: targetUrl,
+        data: datas,
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+        dataType: "json",
+        success: function(data) {
+          console.log(data);
+
+          if (data.code == "1") {
+            $(".modal-body").html(data.msg);
+            // 重置表格
+            $("form")[0].reset();
+          } else {
+            $(".modal-body").html("提交错误，请重试！");
+          }
+        },
+        error: function(err) {
+          $(".models").attr("id", "myModal");
+          console.log(err);
+          $(".modal-body").html("未知错误！");
+        }
+      });
+    } else {
+      $(".models").attr("id", "myModal");
+      $(".modal-body").html("联系人和联系电话必填！");
+    }
   });
   $("#serach").click(function() {
     location.href = "./serviceNet.html";
