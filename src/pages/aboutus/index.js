16c87ac166706cfb49a0ed971aef91f11114820b
吴凯 <wukaiismyh@gmail.com>
2019-10-30 17:00:05 +0800

    [-]去除部分图片

--- src/pages/aboutus/index.js
+++ src/pages/aboutus/index.js
@@ -3,7 +3,7 @@ import "./index.scss";
 import "./bootstrap-paginator";
 import header from "../text/index.txt";
 import footer from "../foot/index.txt";
-$(function() {
+$(function () {
   /*
    * interval为每个轮播项之间的切换时间
    * wrap定义是否循环轮播
@@ -20,9 +20,9 @@ $(function() {
   allReq("资料下载", 1, 10);
 });
 // 封装函数
-var strs = function(data, id) {
+var strs = function (data, id) {
   var str = "";
-  $.each(data, function(ind, item) {
+  $.each(data, function (ind, item) {
     if (ind < 10) {
       str +=
         `<div class="itemBox">
@@ -47,21 +47,29 @@ var strs = function(data, id) {
   $(".newDetailGarid1:last").addClass("newDetailGarid1Last");
 };
 // 封装函数
-var links = function(data, id) {
+var links = function (data, id) {
+  console.log('下载---')
+  console.log(data)
+  console.log(id)
   let items1 = $(id);
-  items1.each(function(index, el) {
-    $(this).on("click", function() {
+  items1.each(function (index, el) {
+    var fileType = data[index].file_path.split('.').pop();
+    console.log(fileType)
+    $(this).on("click", function () {
       const a = document.createElement("a"); // 创建a标签
-      a.setAttribute("download", ""); // download属性
+      const titles = data[index].title + '.' + fileType
+      console.log(titles)
+
+      a.setAttribute("download", titles); // download属性
       a.setAttribute(
         "href",
-        "http://grs.syau.edu.cn/upload/2018/11/%E4%B8%93%E4%B8%9A%E5%AD%A6%E4%BD%8D%E7%A0%94%E7%A9%B6%E7%94%9F%E5%9F%B9%E5%85%BB%E6%89%8B%E5%86%8C%EF%BC%882017%E7%89%88%EF%BC%89.doc"
+        data[index].file_path
       ); // href链接
       a.click(); // 自执行点击事件
     });
   });
 };
-var AjaxPaginator = function(res, obj, ids, page) {
+var AjaxPaginator = function (res, obj, ids, page) {
   var total = res.count / page;
   var options = {
     currentPage: page, //当前页
@@ -69,7 +77,7 @@ var AjaxPaginator = function(res, obj, ids, page) {
     numberOfPages: 5, //设置控件显示的页码数
     bootstrapMajorVersion: 3, //如果是bootstrap3版本需要加此标识，并且设置包含分页内容的DOM元素为UL,如果是bootstrap2版本，则DOM包含元素是DIV
     useBootstrapTooltip: false, //是否显示tip提示框
-    itemTexts: function(type, page, current) {
+    itemTexts: function (type, page, current) {
       //文字翻译
       switch (type) {
         case "first":
@@ -84,7 +92,7 @@ var AjaxPaginator = function(res, obj, ids, page) {
           return page;
       }
     },
-    onPageClicked: function(event, originalEvent, type, pages) {
+    onPageClicked: function (event, originalEvent, type, pages) {
       //给每个页眉绑定一个事件，其实就是ajax请求
       console.log(pages);
       allReq(ids, pages, 10);
@@ -93,7 +101,7 @@ var AjaxPaginator = function(res, obj, ids, page) {
   obj.bootstrapPaginator(options);
 };
 // 全部新闻v
-var allReq = function(ids, page, size) {
+var allReq = function (ids, page, size) {
   var showData = "";
   var tiltw = "";
   if (ids == "资料下载") {
@@ -110,7 +118,7 @@ var allReq = function(ids, page, size) {
     url: new_nndustry,
     type: "GET",
     dataType: "json", //指定服务器返回的数据类型
-    success: function(res) {
+    success: function (res) {
       console.log("资料下载");
       console.log(res);
       strs(res.data.ret, "#newDetailBoxs");
@@ -118,7 +126,7 @@ var allReq = function(ids, page, size) {
       //    设置分页
       AjaxPaginator(res.data, $(".pagination"), ids, page);
     },
-    error: function(XMLHttpRequest, textStatus, errorThrown) {
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
       console.log("请求数据异常，状态码：" + XMLHttpRequest.status);
     }
   });
